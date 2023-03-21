1e7086e89282e5a46ba45372a20b61151203b64b
Edin <edin.fazlic@ssst.edu.ba>
2023-03-21 10:18:28 +0100

    Added: timer in preview component.

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.css']
 })
-export class PreviewComponent {
+export class PreviewComponent implements OnDestroy {
 
+  public formattedTime: string = '';
+  private time: number;
+  private intervalId: number;
+
+  constructor(private router: Router) {
+    this.time = 0;
+    this.intervalId = setInterval(() => {
+      this.time++;
+      this.formattedTime = new Date(this.time * 1000).toISOString().slice(11, 19);
+      console.log(this.formattedTime);
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId)
+  }
+
+
+  navigateFurther() {
+    this.router.navigate([
+      ''
+    ]);
+  }
 }
