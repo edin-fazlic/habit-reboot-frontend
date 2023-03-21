db37d885b5687a8903f33d8b03d5830df53ea040
Edin <edin.fazlic@ssst.edu.ba>
2023-03-21 08:37:29 +0100

    Added: route definitions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PreviewComponent} from './preview/preview.component';
+import {LogsComponent} from './logs/logs.component';
+import {MilestonesComponent} from './milestones/milestones.component';
+import {MilestonesAddComponent} from './milestones-add/milestones-add.component';
+import {MilestonesEditComponent} from './milestones-edit/milestones-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path:'habit123',
+    component:PreviewComponent,
+  },
+  {
+    path:'habit123/logs',
+    component:LogsComponent,
+  },
+  {
+    path:'habit123/milestones',
+    component:MilestonesComponent,
+  },
+  {
+    path:'habit123/milestones/add',
+    component:MilestonesAddComponent,
+  },
+  {
+    path:'habit123/milestones/milestoneId',
+    component:MilestonesEditComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
